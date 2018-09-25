1aef46b50eccf6f31bc345833f02ae247465a611
DESKTOP-3R3N8LF\NexPOS-01 <mena.anthony2812@gmail.com>
2018-09-25 17:35:27 -0400

    Creado servicios de crear,ver y obtencion de usuarios

--- backend/models/models.js
+++ backend/models/models.js
@@ -115,8 +115,94 @@ models.register = (req, res) => {
     });
 };
 
+//Obtener usuarios
+models.getUser = (req, res) => {
+    let data = req.body;
+    console.log(data);
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM usuario WHERE id_empresa = ?', [data.id_empresa], (err, rows) => {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    code: '005',
+                    message: 'Hubo un error en la bd ' + err
+                });
+
+                return;
+            }
+            if (rows.length > 0) {
+                res.status(200).json({
+                    success: true,
+                    code: '002',
+                    data: rows
+                });
+            } else {
+                res.status(400).json({
+                    success: false,
+                    code: '053',
+                    message: "No hay usuarios registrados"
+                });
+            }
+
+        });
+    });
+};
+
+//Asignar Permisologia a usuarios
+models.setPermissions = (req, res) => {
+    console.log("servicio");
+};
+
+//Crear plantillas de permisologia model
+models.createTemplate = (req, res) => {
+    const data = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO plantillas_permisologia( id_empresa, nombre_plantilla, permisologia_ventas, permisologia_compras, permisologia_inventario, permisologia_bancos, permisologia_proveedores, permisologia_pedidos, permisologia_cotizaciones, permisologia_factura_ventas, permisologia_devoluciones_ventas,permisologia_registro_clientes, permisologia_cierre_caja, permisologia_factura_compra, permisologia_orden_compra, permisologia_requisiciones, permisologia_devoluciones_compras, permisologia_proveedor, permisologia_movimientos, permisologia_transferencia, permisologia_cuenta_bancaria, permisologia_conciliacion_banco, permisologia_entradas, permisologia_salida, permisologia_art_serv, permisologia_almacenes, permisologia_existencia, permisologia_cambio_precio) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [data.id_empresa, data.nombre_plantilla, data.perm_ventas, data.perm_compras, data.perm_inventario, data.perm_bancos, data.perm_proveedores, data.perm_pedidos, data.perm_cotizaciones, data.perm_factura_ventas, data.perm_dev_ventas, data.perm_registro_clientes, data.perm_cierre_caja, data.perm_factura_compra, data.perm_orden_compra, data.perm_requisiciones, data.perm_devoluciones_compras, data.perm_pago_proveedores, data.perm_movimientos, data.perm_transferencia, data.perm_cuenta_banc, data.perm_conciliacion_banco, data.perm_entradas, data.perm_salidas, data.perm_art_serv, data.perm_almacenes, data.perm_existencia, data.perm_cambio_precio],
+            (err, rows) => {
+                if (err) {
+                    return res.status(500).json({
+                        success: false,
+                        code: '005',
+                        message: 'Hubo un error en la bd ' + err
+                    });
+                }
+                res.status(201).json({
+                    success: true,
+                    code: '021',
+                    message: 'Plantilla Creada con éxito ',
+                });
+            });
+    });
+};
 
 
+//Obtener plantillas 
+models.getTemplates = (req, res) => {
+    const data = req.body;
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM plantillas_permisologia WHERE id_empresa = ?', [data.id_empresa], (err, rows) => {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    code: '005',
+                    message: 'Hubo un error en la bd ' + err
+                });
+
+                return;
+            }
+            if (rows.length > 0) {
+
+                res.status(200).json({
+                    success: true,
+                    code: '002',
+                    message: 'Plantillas disponibles',
+                    data: rows
+                });
+
+            }
+        });
+    });
+};
 
 //Test
 models.other = (req, res) => {
@@ -126,5 +212,5 @@ models.other = (req, res) => {
         message: "This is other page",
         timestamp: new Date()
     });
-}
+};
 module.exports = models;
--- backend/routes/app.js
+++ backend/routes/app.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const generalController = require('../controllers/generalController');
 var middlewareAuth = require('../middlewares/AuthJWT');
 
+//login - register
 router.post('/login', generalController.login);
 router.post('/register', middlewareAuth.checkToken, generalController.register);
+
+//Get Users
+router.post('/getUsers', middlewareAuth.checkToken, generalController.getUser);
+
+//Set Permission
+router.get('/permissions', generalController.setPermissions);
+
+//Templates
+router.post('/createtemplates', middlewareAuth.checkToken, generalController.createTemplate);
+router.post('/getTemplates', middlewareAuth.checkToken, generalController.getTemplates);
+
+//Test
 router.get('/other', middlewareAuth.checkToken, generalController.other);
 
 

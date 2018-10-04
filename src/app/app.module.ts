43447ca3175efc73afaea15cb9aa42e5778f33f4
Onas Leapage <onas182@gmail.com>
2018-10-04 09:01:05 -0600

    Date time Picker nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
-import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule, MatInputModule, MatSelectModule, MatAutocompleteModule, MatButtonModule } from '@angular/material';
+import {
+  MatFormFieldModule, MatInputModule, MatSelectModule, MatAutocompleteModule, MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSliderModule,
+  MatSlideToggleModule
+} from '@angular/material';
 import { MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatChipsModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DatepickerModule, TimePickerModule, WavesModule } from 'ng-uikit-pro-standard';
+import { MatDatepickerIntl, MatDatepickerModule, MatMomentDateModule } from '@coachcare/datepicker';
+import { DatepickerEsp } from './services/datepicker-esp';
+
 
 import { APP_ROUTING } from './app.routes';
 
 // Componentes
 import 'hammerjs'; // Revisar
 
+import * as moment from 'moment';
+import './services/moment.es';
+moment.locale('es');
+
+
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './components/shared/main-nav/main-nav.component';
 import { MenuTopRightComponent } from './components/shared/menu-top-right/menu-top-right.component';
 import { IndexComponent } from './components/index/index.component';
 import { DateTimeControlsComponent } from './components/date-time-controls/date-time-controls.component';
+import { TablasComponent } from './components/tablas/tablas.component';
 
 @NgModule({
   declarations: [
@@ -29,14 +43,13 @@ import { DateTimeControlsComponent } from './components/date-time-controls/date-
     MenuTopRightComponent,
     IndexComponent,
     DateTimeControlsComponent,
+    TablasComponent,
   ],
   imports: [
     BrowserModule,
     APP_ROUTING,
-    MDBBootstrapModulesPro.forRoot(),
     BrowserAnimationsModule,
     MatFormFieldModule,
-    TimePickerModule,
     MatInputModule,
     MatSelectModule,
     MatAutocompleteModule,
@@ -50,12 +63,19 @@ import { DateTimeControlsComponent } from './components/date-time-controls/date-
     FormsModule,
     MatCardModule,
     MatChipsModule,
-    DatepickerModule,
-    WavesModule
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatDatepickerModule,
+    MatMomentDateModule
 
   ],
   providers: [
-    MDBSpinningPreloader
+    // MDBSpinningPreloader,
+    { provide: MatDatepickerIntl, useClass: DatepickerEsp }
   ],
   bootstrap: [AppComponent]
 })
